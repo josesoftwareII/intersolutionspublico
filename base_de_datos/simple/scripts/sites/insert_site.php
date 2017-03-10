2397c817f0436fa4dc702e84e321f54c9c4b4e88
DavidSanchezRuiz <davidignacio.sanchez@uptc.edu.co>
2017-03-09 22:00:39 -0500

    Correcciones a la base de datos simple

--- base_de_datos/simple/scripts/sites/insert_site.php
+++ base_de_datos/simple/scripts/sites/insert_site.php
@@ -5,9 +5,10 @@ $id_ubicated=$_REQUEST['id_ubicated'];
 $name_site=$_REQUEST['name_site'];
 $cost_site=$_REQUEST['cost_site'];
 $description_site=$_REQUEST['description_site'];
+$link_image_site=$_REQUEST['link_image_site'];
 
-$table="sites(id_ubicated, name_site, cost_site, description_site)";
-$parametros="'$id_ubicated','$name_site','$cost_site','$description_site'";
+$table="sites(id_ubicated, name_site, cost_site, description_site, link_image_site)";
+$parametros="'$id_ubicated','$name_site','$cost_site','$description_site','$link_image_site'";
 
 insert($table,$parametros);
 ?>
