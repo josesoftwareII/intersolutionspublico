fbe610ac04d47d40353ddde9562caeef118811d2
Ivan Espitia <ivan.espitia@uptc.edu.co>
2017-03-14 10:39:09 -0500

    Update select.php
    Modificar retorno.

--- base_de_datos/simple/scripts/lib/select.php
+++ base_de_datos/simple/scripts/lib/select.php
@@ -2,10 +2,6 @@
 function select($select){
 	include 'connect.php';
 	$result = $connection->query($select) or die("Error_".mysqli_error($connection));
-	//@mysql_close($connection);
-	$data=array();
-    foreach ($result as $row) {
-        $data[]=$row;
-    }
-    echo json_encode($data);
+	@mysql_close($connection);
+	return $result;
 }
