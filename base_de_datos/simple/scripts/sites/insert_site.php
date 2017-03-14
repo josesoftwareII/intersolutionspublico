196d1831ae9c8adfad7348ed4bfecedc72075a35
Ivan Espitia <ivan.espitia@uptc.edu.co>
2017-03-14 10:34:41 -0500

    Update insert_site.php
    Prueba

--- base_de_datos/simple/scripts/sites/insert_site.php
+++ base_de_datos/simple/scripts/sites/insert_site.php
@@ -1,14 +1,17 @@
 <?php
 include '../lib/insert.php';
 
-$id_ubicated=$_REQUEST['id_ubicated'];
-$name_site=$_REQUEST['name_site'];
-$cost_site=$_REQUEST['cost_site'];
-$description_site=$_REQUEST['description_site'];
-$link_image_site=$_REQUEST['link_image_site'];
+$arrlength=$_REQUEST['arrlength'];
+$nombre=$_REQUEST['nombre'];
+$descripcion=$_REQUEST['descripcion'];
 
-$table="sites(id_ubicated, name_site, cost_site, description_site, link_image_site)";
-$parametros="'$id_ubicated','$name_site','$cost_site','$description_site','$link_image_site'";
-
-insert($table,$parametros);
+for($x = 0; $x < $arrlength; $x++) {
+    echo $nombre[$x];
+    echo "<br>";
+    echo $descripcion[$x];
+    echo "</br>";
+    $sql .= "INSERT INTO sites (name_site, description_site)
+    VALUES ('$nombre[$x]', '$descripcion[$x]');";
+}
+insert($sql);
 ?>
