5b52584f0b719d0801ec2f8204c17afc6d012407
DavidSanchezRuiz <davidignacio.sanchez@uptc.edu.co>
2017-03-15 22:22:28 -0500

    Scripts.

--- codigo/php/sites/insert_site.php
+++ codigo/php/sites/insert_site.php
@@ -1,11 +1,10 @@
 <?php
 include 'php/lib/insert.php';
-$sql="";
 for($x = 0; $x < $arrlength; $x++) {
-    echo $nombre[$x];
-    echo "<br>";
-    echo $descripcion[$x];
-    echo "</br>";
   insert("INSERT INTO sites (name_site, description_site) VALUES ('$nombre[$x]', '$descripcion[$x]')");
+  echo $nombre[$x];
+  echo "<br>";
+  echo $descripcion[$x];
+  echo "</br>";
 }
 ?>
