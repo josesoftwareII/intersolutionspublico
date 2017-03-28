4b5cf39ce02f979254399907cb005f0f6ed90375
Ivan Espitia <ivan.espitia@uptc.edu.co>
2017-03-28 11:14:09 -0500

    arreglo issue #24
    se arregla problema o bug

--- codigo/index.php
+++ codigo/index.php
@@ -20,7 +20,7 @@
 			<div class="row">
 				<div class="col-md-12">
 					<h3 class="text-center text-muted">
-						BUENOS PLANES
+						BUENOS 
 					</h3>
 					<ul class="nav nav-tabs">
 						<li class="active">
