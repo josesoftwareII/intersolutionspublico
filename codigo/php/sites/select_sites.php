9412e484296426fcbe95fdc0b8c50001a15cc9b7
Ivan Espitia <ivan.espitia@uptc.edu.co>
2017-03-28 22:26:44 -0500

    Corrección código para issue #26
    Se corrige el código de recuperación de datos de la base de datos, en donde se crea un nuevo <div> que contendrá cada fila de datos recuperada de la BD.

--- codigo/php/sites/select_sites.php
+++ codigo/php/sites/select_sites.php
@@ -3,9 +3,12 @@ include 'php/lib/select.php';
 $result=select("SELECT name_site, description_site FROM sites");
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
+        echo "<div class="."card-panel hoverable".">";
+        echo "<img src="."http://www.imagen.com.mx/assets/img/imagen_share.png"." width=200px height=200px></img>";
         echo "<h3>" . "<b>" . $row["name_site"] . "</b>" . "</h3>";
         echo $row["description_site"]. "<br>";
-        echo "<hr></br>";
+        echo "</br>";
+        echo "</div>";
     }
 } else {
     echo "0 results";
